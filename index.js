4845790b9f1c60aea6f2291f2b6784f2cb76bf6c
pellegrineschi <pellegrineschi86@gmail.com>
2024-06-21 11:39:33 -0300

    quite arrancar-proyecto

---
+++ index.js
@@ -0,0 +1,11 @@
+// conexion a bbdd
+
+// crear servidor node
+
+// configurar cors
+
+// convertir los datos del body a objetos js
+
+// cargar conf de rutas
+
+// poner servidor a escuchar peticiones http
