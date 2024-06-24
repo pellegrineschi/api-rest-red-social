378ee8e889bc250d7a8e605346ebf74515e1bae2
pellegrineschi <pellegrineschi86@gmail.com>
2024-06-24 20:40:56 -0300

    rutas

--- controller/user.js
+++ controller/user.js
@@ -1,62 +1,26 @@
 //importar dependencias y modulos
-const user = require("../model/user");
+const User = require("../model/user");
+const bcrypt = require ("bcrypt");
 
-//acciones de prueba
-const pruebaUser = async (req, res)=>{
-    try{
-        return res.status(200).send({
-            message:"Mensaje enviado desde: controller/user.js"
-        })
-    }catch(error){
-        console.log(error);
-    }
+const pruebaUser = async (req, res) => {
+  try {
+    return res.status(200).send({
+      message: "Mensaje enviado desde: controller/user.js",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const checkUser = async(userToSave) =>{
+  try{
+    const user = await User.find({
+      $or: [{email: userToSave.email},{nick: userToSave.nick}],
+      }).exec();
+      
+      return user && user.length >1; 
+
+  }catch(error){
+    console.log(error);   
+  }
 }
-
-//registro de usuarios
-const register = async(req, res)=>{
-
-    //recoger datos de la peticion
-    let params = req.body;
-
-    //comprobar que me llegan bien (+validacion)
-    if(!params.name || !params.email || !params.password || !params.nick){
-
-        return res.status(400).json({
-            status: "error",
-            message: "faltan datos por enviar"
-        })        
-    }
-
-    //crear objeto de usuario
-    let userToSave = new user(params);
-    
-    //control usuarios duplicados
-    user.find({
-        
-    })
-
-    //cifrar contraseñas
-    
-    //guardar usuario en la bbdd
-
-    //devolver resultado
-
-    try{
-        return res.status(200).json({
-            message:"accion de registro de usuarios",
-            params,
-            userToSave
-        });
-
-    }catch(error){
-        console.log(error);
-    }
-    
-}
-
-//exportar aciones
-module.exports ={
-    pruebaUser,
-    register
-} 
-
